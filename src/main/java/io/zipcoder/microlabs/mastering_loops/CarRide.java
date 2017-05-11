d0170c0527ef6bec8afbb47a44ab599e996063f6
Byung-Chun Yoo <byung-chunyoo@zipcoder7.local>
2017-05-11 15:52:42 -0400

    update latest version, still need to complete CarRide

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,22 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Scanner;
+
 public class CarRide {
 
     public String areWeThereYetTest(){
-        return "";
+
+        Scanner in = new Scanner(System.in);
+        String seed = "\" Are we there yet?\"";
+
+        StringBuilder sb = new StringBuilder();
+
+        do {
+            System.out.print(seed);
+            sb.append(in.next());
+
+        }
+        while (in.next() != "Yes");
     }
 
 }
